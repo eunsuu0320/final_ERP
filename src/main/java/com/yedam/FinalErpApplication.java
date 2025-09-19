5b03cf7829a519ac5909410ebf5980b9a75129e7
Choi Eunsu <zxzx320@naver.com>
2025-09-19 13:09:40 +0900

    01

---
+++ src/main/java/com/yedam/FinalErpApplication.java
@@ -0,0 +1,13 @@
+package com.yedam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yedam/FinalErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yedam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
