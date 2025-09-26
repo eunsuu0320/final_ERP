203f2df2c2e42249437edcac600636ab1a18414d
admin <khmin3305@naver.com>
2025-09-26 17:35:20 +0900

    20250926/영업/품목수정/김해민

--- src/main/java/com/yedam/sales1/repository/ProductRepository.java
+++ src/main/java/com/yedam/sales1/repository/ProductRepository.java
@@ -3,28 +3,28 @@ package com.yedam.sales1.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.yedam.sales1.domain.Product;
 
 @Repository
-public interface ProductRepository extends
-		JpaRepository<Product, String>{
+public interface ProductRepository extends JpaRepository<Product, String> {
 
 	List<Product> findAll();
-	
+
 	@Query("SELECT MAX(p.productCode) FROM Product p")
 	String findMaxProductCode();
-	
-	@Query("SELECT p FROM Product p " +
-		       "WHERE (:productName IS NULL OR p.productName = :productName) " +
-		       "AND (:productGroup IS NULL OR p.productGroup = :productGroup) " +
-		       "AND (:warehouseCode IS NULL OR p.warehouseCode = :warehouseCode)")
-		List<Product> findByFilter(
-		        @Param("productName") String productName,
-		        @Param("productGroup") String productGroup,
-		        @Param("warehouseCode") String warehouseCode);
 
+	@Query("SELECT p FROM Product p " + "WHERE (:productName IS NULL OR p.productName = :productName) "
+			+ "AND (:productGroup IS NULL OR p.productGroup = :productGroup) "
+			+ "AND (:warehouseCode IS NULL OR p.warehouseCode = :warehouseCode)")
+	List<Product> findByFilter(@Param("productName") String productName, @Param("productGroup") String productGroup,
+			@Param("warehouseCode") String warehouseCode);
+
+	Product findByProductCode(String productCode);
+	
 }
